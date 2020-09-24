53a971f96ddf023eb5a0ea6696ff00f1878e82cd
oggyrock <steelo.sun@gmail.com>
2020-09-24 14:04:10 +0300

    Updates

---
+++ precache-manifest.4d5679702072a8200bc5180e1148d4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecb0c20f57144f9ba5d0e99a5af624f",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "d1605e26cab2749d92c0",
+    "url": "/todo-app/static/css/main.d183c019.chunk.css"
+  },
+  {
+    "revision": "815e797010b6502bac0a",
+    "url": "/todo-app/static/js/2.fb5ef13c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.fb5ef13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1605e26cab2749d92c0",
+    "url": "/todo-app/static/js/main.6655acad.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
